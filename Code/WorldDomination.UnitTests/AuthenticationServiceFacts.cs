ea520ccd7bb1a69ca6734e5bbb6125e6b8ba3b89
Pure Krome <pure.krome@world-domination.com.au>
2012-10-23 18:00:42 +1100

    - Some AuthenticationService unit test facts. Gotta have coverage!

--- Code/WorldDomination.UnitTests/AuthenticationServiceFacts.cs
+++ Code/WorldDomination.UnitTests/AuthenticationServiceFacts.cs
@@ -60,90 +60,63 @@ namespace WorldDomination.UnitTests
                 const string state = "asd";
                 var authenticationService = new AuthenticationService();
 
-                // Act and Assert
-                var result = authenticationService.RedirectToAuthenticationProvider(providerKey, state);
+                // Act and Assert.
+                var result = Assert.Throws<AuthenticationException>(
+                    () => authenticationService.RedirectToAuthenticationProvider(providerKey, state));
 
+                Assert.NotNull(result);
+                Assert.Equal("No 'aaa' provider has been added.", result.Message);
+            }
+
+            [Fact]
+            public void GivenAValidProviderKey_RedirectToAuthenticate_ReturnsAUri()
+            {
+                // Arrange.
+                var authenticationService = new AuthenticationService();
+                authenticationService.AddProvider(new FacebookProvider("aa", "bb", new Uri("http://www.google.com")));
+
+                // Act.
+                var result = authenticationService.RedirectToAuthenticationProvider("Facebook", "abc");
+
+                // Assert.
+                Assert.NotNull(result);
+                Assert.Equal(result.AbsoluteUri, "https://www.facebook.com/dialog/oauth?client_id=aa&redirect_uri=http://www.google.com/&state=abc");
             }
         }
 
-        //public class CheckCallbackFacts
-        //{
-        //    [Fact]
-        //    public void GiveAnHttpResponseBaseHasNoParams_CheckCallback_ThrowsAnException()
-        //    {
-        //        // Arrange.
-        //        var mockHttpRequestBase = new Mock<HttpRequestBase>();
+        public class CheckCallbackFacts
+        {
+            [Fact]
+            public void GiveAMissingProviderKeyQuerystringValue_CheckCallback_ThrowsAnException()
+            {
+                // Arrange.
+                var authenticationService = new AuthenticationService();
 
-        //        var authenticationService = new AuthenticationService();
+                // Act.
+                var result = Assert.Throws<ArgumentNullException>(() => authenticationService.CheckCallback(null, null, null));
 
-        //        // Act.
-        //        var result = Assert.Throws<AuthenticationException>(() => authenticationService.CheckCallback(mockHttpRequestBase.Object, "meh"));
+                // Assert.
+                Assert.NotNull(result);
+                Assert.Equal("value should not be null or an empty string.\r\nParameter name: value", result.Message);
+            }
 
-        //        // Assert.
-        //        Assert.NotNull(result);
-        //        Assert.Equal("No request params found - unable to determine from where we authenticated with/against.", result.Message);
+            [Fact]
+            public void GivenAnInvalidProviderKey_CheckCallback_ThrowsAnException()
+            {
+                // Arrange.
+                const string providerKey = "aaa";
+                const string state = "asd";
+                var querystringParams = new NameValueCollection();
+                var authenticationService = new AuthenticationService();
 
-        //    }
+                // Act and Assert.
+                var result = Assert.Throws<AuthenticationException>(
+                    () => authenticationService.CheckCallback(providerKey, querystringParams, state));
 
-        //    [Fact]
-        //    public void GivenAFacebookCallback_CheckCallback_RetrievesAnAccessTokenAnUserData()
-        //    {
-        //        // Arrange.
-        //        var mockRestResponseAccessToken = new Mock<IRestResponse>();
-        //        mockRestResponseAccessToken.Setup(x => x.StatusCode).Returns(HttpStatusCode.OK);
-        //        mockRestResponseAccessToken.Setup(x => x.Content).Returns("access_token=foo&expires_on=1000");
-
-        //        var meResult = new MeResult
-        //        {
-        //            Id = 1,
-        //            FirstName = "some firstname",
-        //            LastName = "some lastname",
-        //            Link = "http://whatever",
-        //            Locale = "en-au",
-        //            Name = "Hi there",
-        //            Timezone = 10,
-        //            Username = "PewPew",
-        //            Verified = true
-        //        };
-
-        //        var mockRestResponseApiMe = new Mock<IRestResponse<MeResult>>();
-        //        mockRestResponseApiMe.Setup(x => x.StatusCode).Returns(HttpStatusCode.OK);
-        //        mockRestResponseApiMe.Setup(x => x.Data).Returns(meResult);
-
-        //        var mockRestClient = new Mock<IRestClient>();
-        //        mockRestClient.Setup(x => x.Execute(It.IsAny<IRestRequest>()))
-        //            .Returns(mockRestResponseAccessToken.Object);
-        //        mockRestClient.Setup(x => x.Execute<MeResult>(It.IsAny<IRestRequest>()))
-        //            .Returns(mockRestResponseApiMe.Object);
-
-        //        const string state = "someState";
-        //        var facebookProvider = new FacebookProvider("a", "b", new Uri("http://www.google.com"),
-        //                                                    mockRestClient.Object);
-        //        var authenticationService = new AuthenticationService();
-        //        authenticationService.AddProvider(facebookProvider);
-
-        //        var mockHttpRequestBase = new Mock<HttpRequestBase>();
-        //        mockHttpRequestBase.Setup(x => x.Params)
-        //            .Returns(new NameValueCollection
-        //                     {
-        //                         {"code", "aaaa"},
-        //                         {"state", state}
-        //                     });
-        //        // Act.
-        //        var facebookClient =
-        //            authenticationService.CheckCallback(mockHttpRequestBase.Object, state) as FacebookClient;
-
-
-        //        // Assert.
-        //        Assert.NotNull(facebookClient);
-        //        Assert.NotNull(facebookClient.AccessToken);
-        //        Assert.NotNull(facebookClient.Code);
-        //        Assert.NotNull(facebookClient.UserInformation);
-        //        Assert.True(facebookClient.UserInformation.Id > 0);
-        //        Assert.NotNull(facebookClient.UserInformation.Name);
-        //        Assert.NotNull(facebookClient.UserInformation.UserName);
-        //    }
-        //}
+                Assert.NotNull(result);
+                Assert.Equal("No 'aaa' provider has been added.", result.Message);
+            }
+        }
     }
 
     // ReSharper restore InconsistentNaming
