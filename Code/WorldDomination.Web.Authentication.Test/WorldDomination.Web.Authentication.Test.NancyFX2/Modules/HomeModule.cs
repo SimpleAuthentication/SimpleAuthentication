49c2b463896799474df3ccf0f2acca56072edc59
Phillip Haydon <phillip.haydon@gmail.com>
2013-01-24 21:26:25 +0800

    Added Nancy Provider and NancyFX Test Web App

---
+++ Code/WorldDomination.Web.Authentication.Test/WorldDomination.Web.Authentication.Test.NancyFX2/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Specialized;
+using Nancy;
+using WorldDomination.Web.Authentication.Test.NancyFX2.Model;
+
+namespace WorldDomination.Web.Authentication.Test.NancyFX2.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters => View["login"];
+            Get["/logged-in"] = parameters =>
+            {
+                return "Logged in!!!";
+            };
+        }
+    }
+}
