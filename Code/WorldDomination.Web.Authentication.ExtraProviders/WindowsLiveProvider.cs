4eee6841cb6e55f2764f6aa971f283c322c7f203
Phillip Haydon <phillip.haydon@gmail.com>
2013-02-03 01:51:11 +0800

    Adding WindowsLive provider class

---
+++ Code/WorldDomination.Web.Authentication.ExtraProviders/WindowsLiveProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldDomination.Web.Authentication.ExtraProviders
+{
+    public class WindowsLiveProvider : IAuthenticationProvider
+    {
+        public string Name { get; private set; }
+        public Uri CallBackUri { get; private set; }
+        public IAuthenticationServiceSettings DefaultAuthenticationServiceSettings { get; private set; }
+
+        public Uri RedirectToAuthenticate(IAuthenticationServiceSettings authenticationServiceSettings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAuthenticatedClient AuthenticateClient(NameValueCollection parameters, string existingState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
