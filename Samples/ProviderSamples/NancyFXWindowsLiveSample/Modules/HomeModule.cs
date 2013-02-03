cb2fc787d0baaa9a952d67257b4ac238b8513854
Phillip Haydon <phillip.haydon@gmail.com>
2013-02-03 18:15:36 +0800

    Added Windows Live provider sample

---
+++ Samples/ProviderSamples/NancyFXWindowsLiveSample/Modules/HomeModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Nancy.Authentication.WorldDomination;
+
+namespace NancyFXWindowsLiveSample.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters => View["login"];
+        }
+    }
+
+    public class Test : IAuthenticationCallbackProvider
+    {
+        public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
+        {
+            return nancyModule.Negotiate.WithView("AuthenticateCallback").WithModel(model);
+        }
+    }
+}
