260b10a10e1fdc100a85065fc7810dff873fd5eb
Phillip Haydon <phillip.haydon@gmail.com>
2013-02-05 01:07:34 +0800

    code tidy up

--- Code/WorldDomination.Web.Authentication.ExtraProviders/WindowsLive/WindowsLiveAuthenticationServiceSettings.cs
+++ Code/WorldDomination.Web.Authentication.ExtraProviders/WindowsLive/WindowsLiveAuthenticationServiceSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace WorldDomination.Web.Authentication.ExtraProviders.WindowsLive
+﻿namespace WorldDomination.Web.Authentication.ExtraProviders.WindowsLive
 {
     public class WindowsLiveAuthenticationServiceSettings : BaseAuthenticationServiceSettings
     {
