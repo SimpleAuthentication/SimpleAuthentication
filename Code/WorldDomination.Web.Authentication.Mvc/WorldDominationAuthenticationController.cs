e9fbe2b4c3f88f2e73648d52ae7d046604656a59
Phillip Haydon <phillip.haydon@gmail.com>
2013-02-04 23:28:43 +0800

    added mvc sample project and basic empty files

---
+++ Code/WorldDomination.Web.Authentication.Mvc/WorldDominationAuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace WorldDomination.Web.Authentication.Mvc
+{
+    public class WorldDominationAuthenticationController : Controller
+    {
+        public RedirectResult RedirectToProvider(string providerkey, string additionaldata = null)
+        {
+            return Redirect("");
+        }
+
+        //authentication/redirect
+        public ActionResult AuthenticateCallback(string providerkey)
+        {
+            return Content("Stuffz");
+        }
+    }
+}
