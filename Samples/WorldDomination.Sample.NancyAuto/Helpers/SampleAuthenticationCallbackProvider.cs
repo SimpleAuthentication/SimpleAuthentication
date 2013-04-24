e52de9eac0950c0a53c82c1430d0bea085ac2bd1
Phillip Haydon <phillip.haydon@gmail.com>
2013-04-25 01:05:49 +0800

    Added new sample projects and fixed Nancy

---
+++ Samples/WorldDomination.Sample.NancyAuto/Helpers/SampleAuthenticationCallbackProvider.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Authentication.WorldDomination;
+
+namespace WorldDomination.Sample.NancyAuto.Modules
+{
+    public class SampleAuthenticationCallbackProvider : IAuthenticationCallbackProvider
+    {
+        public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
+        {
+            return nancyModule.Negotiate.WithView("AuthenticateCallback").WithModel(model);
+        }
+
+        public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
+        {
+            var model = new IndexViewModel
+            {
+                ErrorMessage = errorMessage
+            };
+
+            return nancyModule.View["index", model];
+        }
+    }
+}
---
+++ Samples/WorldDomination.Sample.NancyAuto/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace WorldDomination.Sample.NancyAuto.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["index"];
+        }
+    }
+}
