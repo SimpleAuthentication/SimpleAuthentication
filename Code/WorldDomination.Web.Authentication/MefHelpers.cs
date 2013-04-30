996d72b0326e104b68f0a293461e5ebf6e8aa517
Pure Krome <github@world-domination.com.au>
2013-04-30 22:52:03 +1000

    - MefHelpers updated to now do a check for a bin directory, in case the caller is a website instead of self host or unit test.

--- Code/WorldDomination.Web.Authentication/MefHelpers.cs
+++ Code/WorldDomination.Web.Authentication/MefHelpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
 using System.ComponentModel.Composition.Primitives;
 using System.ComponentModel.Composition.ReflectionModel;
+using System.IO;
 using System.Linq;
 
 namespace WorldDomination.Web.Authentication
@@ -12,7 +13,17 @@ namespace WorldDomination.Web.Authentication
         public static IList<Type> GetExportedTypes<T>()
         {
             var path = AppDomain.CurrentDomain.BaseDirectory;
-            var catalog = new AggregateCatalog(new DirectoryCatalog(path, "*"));
+            var catalog = new AggregateCatalog();
+
+            // Handle Self-host or unit tests.
+            catalog.Catalogs.Add(new DirectoryCatalog(path, "*"));
+
+            // Handle WebSites. (ie. the bin directory is a child directory off the root-website directory).
+            var pathWithBin = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin");
+            if (Directory.Exists(pathWithBin))
+            {
+                catalog.Catalogs.Add(new DirectoryCatalog(pathWithBin, "*"));
+            }
 
             return catalog.Parts
                           .Select(part => ComposablePartExportType<T>(part))
