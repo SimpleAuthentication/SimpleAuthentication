ad2098b6e4479bdd61aee574b6d537d4ae0098cb
Phillip Haydon <phillip.haydon@gmail.com>
2013-05-04 00:55:09 +0800

    implemented MVC Manual

--- Samples/WorldDomination.Sample.MvcManual/Controllers/HomeController.cs
+++ Samples/WorldDomination.Sample.MvcManual/Controllers/HomeController.cs
@@ -1,71 +1,12 @@
-﻿using System;
-using System.Web.Mvc;
-using WorldDomination.Sample.MvcManual.Models;
-using WorldDomination.Web.Authentication;
-using WorldDomination.Web.Authentication.Providers;
+﻿using System.Web.Mvc;
 
 namespace WorldDomination.Sample.MvcManual.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IAuthenticationService _authenticationService;
-        
-        public HomeController(IAuthenticationService authenticationService)
-        {
-            _authenticationService = authenticationService;
-        }
-
         public ActionResult Index()
         {
             return View();
-        }
-
-        public RedirectResult RedirectToAuthenticate(string providerKey)
-        {
-            if (string.IsNullOrEmpty(providerKey))
-            {
-                throw new ArgumentNullException("providerKey");
-            }
-
-            // Grab the required Provider settings.
-            var settings = _authenticationService.GetAuthenticateServiceSettings(providerKey, Request.Url, "home/authenticatecallback");
-
-            // Do use a state key.
-            settings.State = null; 
-
-            // Determine the provider's end point Url we need to redirect to.
-            var uri = _authenticationService.RedirectToAuthenticationProvider(settings);
-
-            // Kthxgo!
-            return Redirect(uri.AbsoluteUri);
-        }
-
-        public ActionResult AuthenticateCallback(string providerKey)
-        {
-            if (string.IsNullOrEmpty(providerKey))
-            {
-                throw new ArgumentNullException("providerKey");
-            }
-
-            // Determine which settings we need, based on the Provider.
-            var settings = _authenticationService.GetAuthenticateServiceSettings(providerKey, Request.Url, "home/authenticatecallback");
-
-            // Don't check for somet State.
-            settings.State = null;
-
-            var model = new AuthenticateCallbackViewModel();
-
-            try
-            {
-                // Grab the authenticated client information.
-                model.AuthenticatedClient = _authenticationService.GetAuthenticatedClient(settings, Request.QueryString);
-            }
-            catch (Exception exception)
-            {
-                model.Exception = exception;
-            }
-
-            return View(model);
         }
     }
 }
--- Samples/WorldDomination.Sample.MvcManual/Global.asax.cs
+++ Samples/WorldDomination.Sample.MvcManual/Global.asax.cs
@@ -1,7 +1,11 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 using WorldDomination.Sample.MvcManual.App_Start;
+using WorldDomination.Web.Authentication;
+using WorldDomination.Web.Authentication.Csrf;
 
 namespace WorldDomination.Sample.MvcManual
 {
@@ -13,6 +17,15 @@ namespace WorldDomination.Sample.MvcManual
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<AntiForgery>().As<IAntiForgery>();
+            builder.RegisterType<AuthenticationService>().As<IAuthenticationService>().SingleInstance();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
