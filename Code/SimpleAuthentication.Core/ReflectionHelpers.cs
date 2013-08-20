f2299c19d763bc73fcb778735b5b18cd19c122cc
Phillip Haydon <phillip.haydon@gmail.com>
2013-08-20 23:59:22 +0800

    ripping out MEF cos I hate it

---
+++ Code/SimpleAuthentication.Core/ReflectionHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleAuthentication.Core
+{
+    public static class ReflectionHelpers
+    {
+        public static IList<Type> FindAllTypesOf<T>()
+        {
+            var type = typeof (T);
+
+            return AppDomain.CurrentDomain
+                            .GetAssemblies()
+                            .ToList()
+                            .SelectMany(s => s.GetTypes())
+                            .Where(p => type.IsAssignableFrom(p) && p.IsClass && !p.IsInterface)
+                            .ToList();
+        }
+    }
+}
