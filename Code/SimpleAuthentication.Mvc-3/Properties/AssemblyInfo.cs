585423c5cb11b751221cb5bd32de1e0260d76da9
Pure Krome <github@world-domination.com.au>
2013-09-11 00:22:33 +1000

    Added a new MVC 3 project.

--- Code/SimpleAuthentication.Mvc-3/Properties/AssemblyInfo.cs
+++ Code/SimpleAuthentication.Mvc-3/Properties/AssemblyInfo.cs
@@ -1,22 +1,26 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Shared Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyCompany("SimpleAuthentication")]
-[assembly: AssemblyProduct("SimpleAuthentication")]
-[assembly: AssemblyCopyright("Copyright © 2012-2013")]
+[assembly: AssemblyTitle("SimpleAuthentication.Mvc-3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleAuthentication.Mvc-3")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e30e328-2889-4ba7-85e5-09767c7f9ff4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.6.0")]
-[assembly: AssemblyFileVersion("0.3.6.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
