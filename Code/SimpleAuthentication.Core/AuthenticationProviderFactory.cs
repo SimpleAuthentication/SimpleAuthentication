424ac65cc840f3059473d4c4407fb7cdb4b876de
Phillip Haydon <phillip.haydon@gmail.com>
2013-09-19 00:51:22 +0800

    refactored the method to be not so crap

--- Code/SimpleAuthentication.Core/AuthenticationProviderFactory.cs
+++ Code/SimpleAuthentication.Core/AuthenticationProviderFactory.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using SimpleAuthentication.Core.Config;
-using SimpleAuthentication.Core.Tracing;
-
-namespace SimpleAuthentication.Core
+﻿namespace SimpleAuthentication.Core
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Config;
+    using Tracing;
+
     public class AuthenticationProviderFactory
     {
         private static readonly Lazy<IDictionary<string, IAuthenticationProvider>> Providers =
@@ -64,7 +64,7 @@ namespace SimpleAuthentication.Core
         }
 
         private static void SetupAppSettingsConfigProviders(
-            IDictionary<string, IAuthenticationProvider> authenticationProviders, 
+            IDictionary<string, IAuthenticationProvider> authenticationProviders,
             IList<Type> discoveredProviders,
             IList<AppSettingsParser.ProviderKey> appSettings)
         {
@@ -97,13 +97,12 @@ namespace SimpleAuthentication.Core
                 AddProviderToDictionary(authenticationProviders, discoveredProvider, false);
             }
         }
-        
-        private static IAuthenticationProvider DiscoverProvider(IList<Type> discoveredProviders, AppSettingsParser.ProviderKey providerKey)
+
+        public static IAuthenticationProvider DiscoverProvider<T>(IList<Type> discoveredProviders,
+            string name,
+            Func<T> providerParamsFunc) where T : ProviderParams
         {
             discoveredProviders.ThrowIfNull("discoveredProviders");
-            providerKey.ThrowIfNull("providerKey");
-
-            var name = providerKey.ProviderName.ToLowerInvariant();
 
             var provider = discoveredProviders.SingleOrDefault(x => x.Name.ToLowerInvariant().StartsWith(name));
 
@@ -113,7 +112,7 @@ namespace SimpleAuthentication.Core
                     string.Format(
                         "Unable to find the provider [{0}]. Is there a provider dll available? Is there a typo in the provider name? Solution suggestions: Check to make sure the correct dll's are in the 'bin' directory and/or check the name to make sure there's no typo's in there. Example: If you're trying include the GitHub provider, make sure the name is 'github' (any case) and that the ExtraProviders dll exists in the 'bin' directory or make sure you've downloaded the package via NuGet -> install-package SimpleAuthentication.ExtraProviders.",
                         name);
-                //TraceSource.TraceError(errorMessage);
+                
                 throw new ApplicationException(errorMessage);
             }
 
@@ -124,15 +123,7 @@ namespace SimpleAuthentication.Core
             // that has a sigle ProviderParams, then we're stuffed. So we need to help them.
             if (provider.GetConstructor(new[] {typeof (ProviderParams)}) != null)
             {
-                var parameters = new ProviderParams
-                {
-                    PublicApiKey = providerKey.Key,
-                    SecretApiKey = providerKey.Secret,
-                    Scopes = string.IsNullOrEmpty(providerKey.Scope)
-                        ? null
-                        : providerKey.Scope.Split(new[] {','},
-                            StringSplitOptions.RemoveEmptyEntries)
-                };
+                var parameters = providerParamsFunc();
 
                 authenticationProvider = Activator.CreateInstance(provider, parameters) as IAuthenticationProvider;
             }
@@ -144,65 +135,47 @@ namespace SimpleAuthentication.Core
                     string.Format(
                         "The type {0} doesn't have the proper constructor. It requires a constructor that only accepts 1 argument of type ProviderParams. Eg. public MyProvider(ProviderParams providerParams){{ .. }}.",
                         provider.FullName);
-                //TraceSource.TraceError(errorMessage);
+
                 throw new ApplicationException(errorMessage);
             }
 
             return authenticationProvider;
         }
 
-        private static IAuthenticationProvider DiscoverProvider(IEnumerable<Type> discoveredProviders,
-                                                                ProviderKey providerKey)
+        /// <summary>
+        /// Method used when using appSettings section
+        /// </summary>
+        private static IAuthenticationProvider DiscoverProvider(IList<Type> discoveredProviders,
+            AppSettingsParser.ProviderKey providerKey)
         {
-            discoveredProviders.ThrowIfNull("discoveredProviders");
+            providerKey.ThrowIfNull("providerKey");
+
+            var name = providerKey.ProviderName.ToLowerInvariant();
+
+            return DiscoverProvider(discoveredProviders, name, () => new ProviderParams
+            {
+                PublicApiKey = providerKey.Key,
+                SecretApiKey = providerKey.Secret,
+                Scopes = providerKey.Scope.ScopesToCollection()
+            });
+        }
+
+        /// <summary>
+        /// Method used when using custom config section
+        /// </summary>
+        private static IAuthenticationProvider DiscoverProvider(IList<Type> discoveredProviders,
+            ProviderKey providerKey)
+        {
             providerKey.ThrowIfNull("providerKey");
 
             var name = providerKey.Name.ToLowerInvariant();
 
-            var provider = discoveredProviders.SingleOrDefault(x => x.Name.ToLowerInvariant().StartsWith(name));
-
-            if (provider == null)
+            return DiscoverProvider(discoveredProviders, name, () => new ProviderParams
             {
-                var errorMessage =
-                    string.Format(
-                        "Unable to find the provider [{0}]. Is there a provider dll available? Is there a typo in the provider name? Solution suggestions: Check to make sure the correct dll's are in the 'bin' directory and/or check the name to make sure there's no typo's in there. Example: If you're trying include the GitHub provider, make sure the name is 'github' (any case) and that the ExtraProviders dll exists in the 'bin' directory or make sure you've downloaded the package via NuGet -> install-package SimpleAuthentication.ExtraProviders.",
-                        name);
-                //TraceSource.TraceError(errorMessage);
-                throw new ApplicationException(errorMessage);
-            }
-
-            IAuthenticationProvider authenticationProvider = null;
-
-            // Make sure we have a provider with the correct constructor parameters.
-            // How? If a person creates their own provider and doesn't offer a constructor
-            // that has a sigle ProviderParams, then we're stuffed. So we need to help them.
-            if (provider.GetConstructor(new[] { typeof(ProviderParams) }) != null)
-            {
-                var parameters = new ProviderParams
-                {
-                    PublicApiKey = providerKey.Key,
-                    SecretApiKey = providerKey.Secret,
-                    Scopes = string.IsNullOrEmpty(providerKey.Scope)
-                        ? null
-                        : providerKey.Scope.Split(new[] { ',' },
-                            StringSplitOptions.RemoveEmptyEntries)
-                };
-
-                authenticationProvider = Activator.CreateInstance(provider, parameters) as IAuthenticationProvider;
-            }
-
-            if (authenticationProvider == null)
-            {
-                // We didn't find a proper constructor for the provider class we wish to instantiate.
-                var errorMessage =
-                    string.Format(
-                        "The type {0} doesn't have the proper constructor. It requires a constructor that only accepts 1 argument of type ProviderParams. Eg. public MyProvider(ProviderParams providerParams){{ .. }}.",
-                        provider.FullName);
-                //TraceSource.TraceError(errorMessage);
-                throw new ApplicationException(errorMessage);
-            }
-
-            return authenticationProvider;
+                PublicApiKey = providerKey.Key,
+                SecretApiKey = providerKey.Secret,
+                Scopes = providerKey.Scope.ScopesToCollection()
+            });
         }
 
         private static void AddProviderToDictionary(
@@ -228,8 +201,7 @@ namespace SimpleAuthentication.Core
         }
 
         private static void RemoveProviderFromDictionary(string providerName,
-                                                         IDictionary<string, IAuthenticationProvider>
-                                                             authenticationProviders)
+            IDictionary<string, IAuthenticationProvider> authenticationProviders)
         {
             providerName.ThrowIfNull("providerName");
             authenticationProviders.ThrowIfNull("authenticationProviders");
@@ -246,6 +218,16 @@ namespace SimpleAuthentication.Core
             {
                 throw new ArgumentNullException(parameter);
             }
+        }
+
+        public static ICollection<string> ScopesToCollection(this string scopes)
+        {
+            if (string.IsNullOrEmpty(scopes))
+            {
+                return null;
+            }
+
+            return scopes.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
