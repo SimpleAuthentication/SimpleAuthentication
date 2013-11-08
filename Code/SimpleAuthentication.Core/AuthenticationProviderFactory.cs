96db0f0cb09ac88f2d2b23ec90d867afc1416071
Phillip Haydon <phillip.haydon@gmail.com>
2013-11-09 00:14:35 +0800

    obsolete the old delete function and implement new one

--- Code/SimpleAuthentication.Core/AuthenticationProviderFactory.cs
+++ Code/SimpleAuthentication.Core/AuthenticationProviderFactory.cs
@@ -6,6 +6,8 @@ using SimpleAuthentication.Core.Tracing;
 
 namespace SimpleAuthentication.Core
 {
+    using Annotations;
+
     public class AuthenticationProviderFactory
     {
         public static Lazy<Configuration> Configuration =
@@ -30,15 +32,23 @@ namespace SimpleAuthentication.Core
             get { return Providers.Value; }
         }
 
-        public ITraceManager TraceManager { set; private get; }
+        public ITraceManager TraceManager { set; [UsedImplicitly] private get; }
 
         public void AddProvider(IAuthenticationProvider provider, bool replaceExisting = true)
         {
             AddProviderToDictionary(AuthenticationProviders, provider, replaceExisting);
         }
 
+        [Obsolete("Use RemoveProvider<T>(), this will be removed in a future version.")]
         public void RemoveProvider(string providerName)
         {
+            RemoveProviderFromDictionary(providerName, AuthenticationProviders);
+        }
+
+        public void RemoveProvider<T>()
+        {
+            var providerName = typeof(T).Name.ToLowerInvariant();
+
             RemoveProviderFromDictionary(providerName, AuthenticationProviders);
         }
 
@@ -122,7 +132,7 @@ namespace SimpleAuthentication.Core
             // Make sure we have a provider with the correct constructor parameters.
             // How? If a person creates their own provider and doesn't offer a constructor
             // that has a sigle ProviderParams, then we're stuffed. So we need to help them.
-            if (provider.GetConstructor(new[] {typeof (ProviderParams)}) != null)
+            if (provider.GetConstructor(new[] { typeof(ProviderParams) }) != null)
             {
                 var parameters = providerParamsFunc();
 
@@ -150,25 +160,24 @@ namespace SimpleAuthentication.Core
             var name = provider.Name.ToLowerInvariant();
 
             return DiscoverProvider(discoveredProviders, name, () => new ProviderParams
-                                                                     {
-                                                                         PublicApiKey = provider.Key,
-                                                                         SecretApiKey = provider.Secret,
-                                                                         Scopes = provider.Scopes.ScopesToCollection()
-                                                                     });
+            {
+                PublicApiKey = provider.Key,
+                SecretApiKey = provider.Secret,
+                Scopes = provider.Scopes.ScopesToCollection()
+            });
         }
 
         private static void AddProviderToDictionary(
             IDictionary<string, IAuthenticationProvider> authenticationProviders,
             IAuthenticationProvider provider,
-            bool replaceExisting = true)
+            [UsedImplicitly] bool replaceExisting = true)
         {
             provider.ThrowIfNull("provider");
             authenticationProviders.ThrowIfNull("authenticationProviders");
 
             var key = provider.Name.ToLower();
 
-            if (authenticationProviders.ContainsKey(key) &&
-                !replaceExisting)
+            if (authenticationProviders.ContainsKey(key) && !replaceExisting)
             {
                 throw new InvalidOperationException(
                     string.Format(
@@ -179,14 +188,19 @@ namespace SimpleAuthentication.Core
             authenticationProviders[key] = provider;
         }
 
-        private static void RemoveProviderFromDictionary(string providerName,
-                                                         IDictionary<string, IAuthenticationProvider>
-                                                             authenticationProviders)
+        private static void RemoveProviderFromDictionary(string providerName, ICollection<KeyValuePair<string, IAuthenticationProvider>> authenticationProviders)
         {
             providerName.ThrowIfNull("providerName");
             authenticationProviders.ThrowIfNull("authenticationProviders");
 
-            authenticationProviders.Remove(providerName);
+            var providersToRemove =
+                authenticationProviders.Where(x => providerName.StartsWith(x.Key.ToLowerInvariant()))
+                                       .ToList();
+
+            foreach (var providerPair in providersToRemove)
+            {
+                authenticationProviders.Remove(providerPair);
+            }
         }
     }
 
@@ -207,7 +221,7 @@ namespace SimpleAuthentication.Core
                 return null;
             }
 
-            return scopes.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+            return scopes.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
