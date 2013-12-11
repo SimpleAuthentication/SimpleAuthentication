415f3d9e17d9fec9c1955a080ed908477f5138c8
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-12-11 09:53:58 +0100

    Implemented InstagramProvider.

---
+++ Code/SimpleAuthentication.ExtraProviders/Instagram/UserInfoResult.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleAuthentication.ExtraProviders.Instagram
+{
+    public class UserInfoResult
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Bio { get; set; }
+        public string Website { get; set; }
+    }
+}
