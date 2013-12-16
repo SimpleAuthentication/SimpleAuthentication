112b71f4a0cd63e4551b189c6cb09842757f012c
Phillip Haydon <phillip.haydon@gmail.com>
2013-12-17 01:04:28 +0800

    added bat file for easier packaging and release 0.3.10

---
+++ NuGet Package Specifications/package-nugets.bat
@@ -0,0 +1,10 @@
+echo off
+set version=%1
+shift
+
+nuget.exe pack Glimpse.SimpleAuthentication.nuspec -Version %version%
+nuget.exe pack Nancy.SimpleAuthentication.nuspec -Version %version%
+nuget.exe pack SimpleAuthentication.Core.nuspec -Version %version%
+nuget.exe pack SimpleAuthentication.ExtraProviders.nuspec -Version %version%
+nuget.exe pack SimpleAuthentication.Mvc-3.nuspec -Version %version%
+nuget.exe pack SimpleAuthentication.Mvc-4.nuspec -Version %version%
