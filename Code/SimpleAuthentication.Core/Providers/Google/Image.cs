5de8698714603205ff5e662577d7c40f6cc77920
Pure Krome <github@world-domination.com.au>
2015-10-21 00:12:15 +1100

    Updated Google's OAuth 2.0 implementation. - Scopes values were deprecated. Now using the latest scopes. - New 'Me' endpoint and JSON payload.

---
+++ Code/SimpleAuthentication.Core/Providers/Google/Image.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleAuthentication.Core.Providers.Google
+{
+    public class Image
+    {
+        public string Url { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
---
+++ Code/SimpleAuthentication.Core/Providers/Google/Name.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleAuthentication.Core.Providers.Google
+{
+    public class Name
+    {
+        public string FamilyName { get; set; }
+        public string GivenName { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", GivenName, FamilyName).Trim();
+        }
+    }
+}
