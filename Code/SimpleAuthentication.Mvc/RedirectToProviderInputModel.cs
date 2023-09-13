a43566875ec8127ff582b595ca68af51c79187bd
Pure Krome <github@world-domination.com.au>
2023-09-14 00:04:22 +1000

    🔥🚮

--- Code/SimpleAuthentication.Mvc/RedirectToProviderInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleAuthentication.Mvc
-{
-    public class RedirectToProviderInputModel
-    {
-        public string ProviderName { get; set; }
-        public string Identifier { get; set; }
-        public string ReturnUrl { get; set; }
-    }
-}
