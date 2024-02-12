00de6ee08f212e168af44288573997c9a204aabc
Jack W Finlay <jack.finlay@homely.com.au>
2024-02-12 16:16:16 +1100

    Add google prompt account selection

---
+++ Code/SimpleAuthentication.Core/Providers/Google/GoogleProviderParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleAuthentication.Core.Providers.Google
+{
+    public class GoogleProviderParams : ProviderParams
+    {
+        public string PromptType { get; set; }
+    }
+}
